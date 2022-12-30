f33987ed93651b135622afc3cf9e6cf7fd32d772
chrsita <sita.christiani@tokopedia.com>
2022-12-30 14:04:37 +0700

    update

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -2,5 +2,6 @@ public class HelloWorld {
     public static void main(String[] args){
         System.out.println("Hello World!");
         System.out.println("Hello Sita, this is your first Java Project!");
+        System.out.println("There is an update");
     }
 }
