0ac8e7fd61ebdb0d845e6a34a6f43e90ee0aa154
christianisita <sitachristiani@gmail.com>
2022-12-30 14:10:53 +0700

    revert update

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -2,6 +2,5 @@ public class HelloWorld {
     public static void main(String[] args){
         System.out.println("Hello World!");
         System.out.println("Hello Sita, this is your first Java Project!");
-        System.out.println("There is an update");
     }
 }
